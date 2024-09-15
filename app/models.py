1230604935dfbcc2823d822c05ded6dbcd056ba8
Natalia Chernenko <natchernenko13@gmail.com>
2024-09-15 21:07:20 +0800

    Delete db models

--- app/models.py
+++ app/models.py
@@ -6,16 +6,12 @@ class Teacher(db.Model):
     name = db.Column(db.String(64), nullable=False)
     subject = db.Column(db.String(64))
     total_weekly_hours = db.Column(db.Integer, default=0)
-    cover_history = db.Column(db.Integer, default=0)
-    schedules = db.relationship('TeachingSchedule', backref='teacher', lazy=True)
 
 
 class TeachingSchedule(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'), nullable=False)
     day_of_week = db.Column(db.String(10))
-    start_time = db.Column(db.Time, nullable=False)
-    end_time = db.Column(db.Time, nullable=False)
     is_free_period = db.Column(db.Boolean, default=False)
 
 
@@ -32,5 +28,3 @@ class CoverAssignment(db.Model):
     absent_teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'), nullable=False)
     covering_teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'), nullable=False)
     date = db.Column(db.Date, nullable=False)
-    start_time = db.Column(db.Time, nullable=False)
-    end_time = db.Column(db.Time, nullable=False)
